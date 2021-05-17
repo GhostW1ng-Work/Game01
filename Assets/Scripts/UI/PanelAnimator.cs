b37b5f878705dacaa24c31461e4079424c979782
Shestakov Nicolay <justghostw1ng@gmail.com>
2021-05-17 17:25:53 +0700

    Create an animation of show developers when clicking on a buttons

--- Assets/Scripts/UI/PanelAnimator.cs
+++ Assets/Scripts/UI/PanelAnimator.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class PanelAnimator : MonoBehaviour
 {
     private const string Open = "Open"; 
+
     [SerializeField] private Animator _animator;
 
     private void Start()
